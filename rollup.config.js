79f85dcd72914e773d956d8ed79b070a113a2ea4
Kjell <kjellapps@gmail.com>
2021-08-20 06:30:51 -0700

    adding library

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,7 @@ if you want to view the source visit the plugins github repository
 
 export default [
     {
-    input: 'src/main.ts',
+    input: 'src/plugin/main.ts',
     output: {
       dir: '.',
       sourcemap: 'inline',
